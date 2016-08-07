2dc7cec976784d5c1771f8f4bc4ee35f67cb22de
Basuke Suzuki <basuke@kinoma.com>
2016-08-07 10:58:04 -0700

    fix2

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import sys
 from bottle import route, run, template
 
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-bottle.run(host='0.0.0.0', port=argv[1])
+run(host='0.0.0.0', port=sys.argv[1])
